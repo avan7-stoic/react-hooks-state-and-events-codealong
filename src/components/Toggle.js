31be27e8519cf955cf1ed51cb2b6c197c9b93060
avant muchiri <avant.muchiri@student.moringaschool>
2024-08-08 13:35:30 +0300

    components

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
+    const [isOn, setIsOn] = useState(false);
+    function handleClick() {
+      setIsOn((isOn) => !isOn);
+    }
+    <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>
   return <button>OFF</button>;
 }
 
